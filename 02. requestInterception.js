56d2d4a365ee8d4b87d3dd95d404993b165c7787
Alex Cheung <alexkwokoncheung@AC.local>
2022-11-10 08:02:49 -0800

    added 02. requestInterception.js

---
+++ 02. requestInterception.js
@@ -0,0 +1,29 @@
+import puppeteer from 'puppeteer';
+
+// Request interception enables us to observe which requests and responses are being exchanged as part of our script’s execution
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 250,
+    devtools: true,
+  });
+
+  const page = await browser.newPage();
+
+  await page.setRequestInterception(true);
+
+  page.on('request', (request) => {
+    console.log('>>', request.method(), request.url());
+    request.continue();
+  });
+
+  page.on('response', (response) => {
+    console.log('<<', response.status(), response.url());
+  });
+
+  await page.goto('https://danube-web.shop/');
+
+  await page.screenshot({ path: 'screenshot.png' });
+
+  await browser.close();
+})();
